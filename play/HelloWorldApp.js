65d15e4f4dabad371470aca8daeb1cec1595c236
Reto Gmür <reto@factsmission.com>
2020-08-03 10:50:53 +0200

    code to publish app

---
+++ play/HelloWorldApp.js
@@ -0,0 +1,10 @@
+"use strict";
+const html = `
+  <body>
+    <h1>Hello World</h1>
+
+    <div>This is a PatchBoot App</div>
+  </body>
+</html>`;
+console.log('Hello world');
+//console.log('window', window)
---
+++ play/install-example-app.js
@@ -0,0 +1,34 @@
+const Connection = require("ssb-client");
+const pull = require("pull-stream");
+const fs = require("fs");
+const path = require("path");
+Connection(async (err, server) => {
+    if (err) {
+        console.log('could not get keys, got err', err);
+    }
+    else {
+        console.log('hj ' + server);
+        console.log(server.publish);
+        var script = fs.readFileSync(path.join(__dirname, '.', 'HelloWorldApp.js'), 'utf8');
+        console.log('response', script);
+        pull(pull.values([script]), server.blobs.add(undefined, function (err, hash) {
+            console.log('cb ', arguments);
+            server.publish({
+                type: 'patchboot-app',
+                comment: 'A hello world app',
+                mentions: [
+                    {
+                        'link': hash,
+                        'name': 'Hello World App',
+                        'type': 'app'
+                    }
+                ]
+            }, function (err, msg) {
+                console.log(msg);
+                server.close();
+            });
+            console.log("done");
+        }));
+    }
+});
+console.log("after connection");
