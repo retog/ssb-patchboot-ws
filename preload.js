259932fd2e9b9b92bcdc1c693a99adb54de98d25
Reto Gmür <reto@factsmission.com>
2020-08-04 20:25:12 +0200

    wanting blobs

--- preload.js
+++ preload.js
@@ -31,8 +31,6 @@ window.addEventListener('DOMContentLoaded', () => {
       const element = document.getElementById('apps')
       const view = document.getElementById('view')
       const shadowView = view.attachShadow({mode: 'closed'});
-      console.log('monitoring', server);
-      console.log(server.blobs.ls());
       const opts = {
         reverse: true,
         query: [
@@ -51,6 +49,7 @@ window.addEventListener('DOMContentLoaded', () => {
           return;
         }
         if (msg.value.content.type == 'patchboot-app') {
+          console.log('app', msg.value);
           const link = document.createElement('div')
           link.classList.add('block', 'app')
           link.innerHTML = `<h2>${msg.value.content.mentions[0].name||msg.value.content.comment||''}</h2>
@@ -60,17 +59,19 @@ window.addEventListener('DOMContentLoaded', () => {
           const blobId = msg.value.content.mentions[0].link;
           link.addEventListener('click', () => { 
             Connection((err, server) => {
-              pull(
-                server.blobs.get(blobId), 
-                pull.collect(function (err, values) {
-                  if (err) throw err
-                  const code = values.join('')
-                  console.log('executing', code);
-                  window.setTimeout(() => {
-                    const fun = new Function('root', 'ssb', code);
-                    fun(shadowView, Connection);
-                  }, 0);
-              }))
+              server.blobs.want(blobId).then(() => {
+                pull(
+                  server.blobs.get(blobId), 
+                  pull.collect(function (err, values) {
+                    if (err) throw err
+                    const code = values.join('')
+                    console.log('executing', code);
+                    window.setTimeout(() => {
+                      const fun = new Function('root', 'ssb', code);
+                      fun(shadowView, Connection);
+                    }, 0);
+                }))
+              });
             })
           })
           element.append(link)
@@ -78,7 +79,6 @@ window.addEventListener('DOMContentLoaded', () => {
       }, function (end) {
         console.log("ending with " + end);
       }));
-      console.log("done");
     }
     //server.close();
   });
