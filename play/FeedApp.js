64ecd9e47db65590bf578841800b6cdbd3dfd07b
Reto Gmür <reto@factsmission.com>
2020-08-13 23:29:56 +0200

    doesn't work when accesing shared sbot!

--- play/FeedApp.js
+++ play/FeedApp.js
@@ -1,25 +1,19 @@
-ssb((err, server) => {
-  if (err) throw err
+const opts = {
+  limit: 100,
+  reverse: true
+}
 
-  const opts = {
-    limit: 100,
-    reverse: true
-  }
-
+ssb((err, sbot) => {
   pull(
-    server.query.read(opts),
+    sbot.query.read(opts),
     pull.drain(receive)
   )
-
-  function receive(msg) {
-    if (err) {
-      console.error(err)
-      server.close()
-      return
-    }
-    render(msg)
-  }
 })
+
+function receive(msg) {
+  render(msg)
+}
+
 
 function render(msg) {
   root.innerHTML += `<textarea rows="16" cols="80">${JSON.stringify(msg, null, 2)}</textarea>`
