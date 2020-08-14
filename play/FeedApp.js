d3d230d820949697c095dd48006d3cc2828ceca5
Reto Gmür <reto@factsmission.com>
2020-08-14 14:20:29 +0200

    it seemsto work with direct access to sbot

--- play/FeedApp.js
+++ play/FeedApp.js
@@ -3,18 +3,11 @@ const opts = {
   reverse: true
 }
 
-ssb((err, sbot) => {
-  pull(
-    sbot.query.read(opts),
-    pull.drain(receive)
-  )
-})
+pull(
+  sbot.query.read(opts),
+  pull.drain(receive)
+)
 
 function receive(msg) {
-  render(msg)
-}
-
-
-function render(msg) {
   root.innerHTML += `<textarea rows="16" cols="80">${JSON.stringify(msg, null, 2)}</textarea>`
 }
