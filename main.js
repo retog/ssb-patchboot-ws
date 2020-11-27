e0933dd5528e98702af5e259c20b74cb6477954c
Reto Gmür <reto@factsmission.com>
2020-11-27 17:53:36 +0100

    usig expected name, fixed path for other cwd

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const path = require('path')
 const { EntryPage } = require('./lib/EntryPage.js')
 
 module.exports = {
-  name: 'ssb-patchboot-ws',
+  name: 'patchboot-ws',
   version: '0.0.1',
   manifest: {},
   init: function (sbot) {
-    console.log('initializing patchboot')
+    console.log(`initializing patchboot at http:${sbot.config.ws.host}:${sbot.config.ws.port}/patchboot/`)
     const serverPublicKey = sbot.id.substring(1)
     const entryPage = new EntryPage(serverPublicKey)
     sbot.ws.use(function (req, res, next) {
-      console.log('handling', req.url)
       if (req.url === '/patchboot') {
         res.statusCode = 301
         res.setHeader('Location', '/patchboot/')
@@ -20,9 +19,10 @@ module.exports = {
         entryPage.render(req, res)
       } else if(req.url.startsWith('/patchboot/')) {
         const subpath = req.url.substring(11)
-        const filepath = path.join('public', subpath)
+        const filepath = path.join(__dirname,'public', subpath)
         fs.readFile(filepath, (err, data) => {
-          res.end(data)
+          if (err) console.log(`${err} getting ${filepath}`)
+          else res.end(data)
         })
       } else next()
     })
