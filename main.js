4f376662217390348c099fc2d183d41beecd8ab3
Reto Gmür <reto@factsmission.com>
2020-11-27 17:08:41 +0100

    cleaned

--- main.js
+++ main.js
@@ -28,24 +28,3 @@ module.exports = {
     })
   }
 }
-
-
-
-  
-/*
-
-  app.use(express.static('public'))
-
-  // on the request to root (localhost:3000/)
-  app.get('/', (req, res) => entryPage.render(req, res));
-
-  // Change the 404 message modifing the middleware
-  app.use(function(req, res, next) {
-      res.status(404).send("Nothing here.");
-  });
-
-  // start the server in the port 3000 !
-  app.listen(3000, function () {
-      console.log('Example app listening on port 3000.');
-  });
-*/
